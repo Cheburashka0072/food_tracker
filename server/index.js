af52ea3170ec7d7ba5da11074b1337d9ae266dca
Richard <yarik271202@gmail.com>
2024-06-10 19:46:34 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 require("dotenv").config();
-const config = require("config");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
 
 app.use(express.json({ extended: true }));
-
 app.use(cors());
+
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/profile", require("./routes/profile.routes"));
 app.use("/api/stat", require("./routes/stat.routes"));
@@ -16,8 +15,24 @@ app.use("/api/dish", require("./routes/dish.routes"));
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// Подключение к MongoDB
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB connected!");
 
-app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
+        app.listen(PORT, () => console.log(`Server ready on port ${PORT}.`));
+    } catch (err) {
+        console.error("Server Error", err.message);
+        process.exit(1);
+    }
+};
+
+start();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 module.exports = app;
